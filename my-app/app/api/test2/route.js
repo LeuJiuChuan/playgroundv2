8c6cbf690fb822b623503e4919808da606207348
LeuJiuChuan <jiuc0474@gmail.com>
2023-11-06 10:46:53 +0800

    UI:Using emoji in your image

--- my-app/app/api/test2/route.js
+++ my-app/app/api/test2/route.js
@@ -5,28 +5,30 @@ import { ImageResponse } from 'next/og';
 export const runtime = 'edge';
  
 export async function GET() {
-  const imageData = await fetch(new URL('/public/click.jpg', import.meta.url)).then(
-    (res) => res.arrayBuffer(),
-  );
   return new ImageResponse(
     (
       <div
         style={{
-          display: 'flex',
-          background: '#f6f6f6',
+          fontSize: 100,
+          color: 'black',
+          background: 'white',
           width: '100%',
           height: '100%',
-          flexDirection: 'column',
+          padding: '50px 200px',
+          textAlign: 'center',
           justifyContent: 'center',
           alignItems: 'center',
         }}
       >
-        <img width="256" height="256" src={imageData} />
+        👋, 🌎
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      // Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent' and 'fluentFlat'
+      // Default to 'twemoji'
+      emoji: 'noto',
     },
   );
 }
